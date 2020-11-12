4cf1364bf95a0f2255eacd60e9f20a0b55e055a7
João Fellipe <felippe.uller@gmail.com>
2020-11-11 21:15:51 -0300

    Adjusted APPs Traits

--- app/producer_consumer/producer_consumer_traits.h
+++ app/producer_consumer/producer_consumer_traits.h
@@ -15,7 +15,7 @@ template<> struct Traits<Build>: public Traits_Tokens
     static const unsigned int MODEL = SiFive_E;
     static const unsigned int CPUS = 1;
     static const unsigned int NODES = 1; // (> 1 => NETWORKING)
-    static const unsigned int EXPECTED_SIMULATION_TIME = 60; // s (0 => not simulated)
+    static const unsigned int EXPECTED_SIMULATION_TIME = 30; // s (0 => not simulated)
 
     // Default flags
     static const bool enabled = true;
@@ -111,7 +111,7 @@ template<> struct Traits<System>: public Traits<Build>
     static const unsigned long LIFE_SPAN = 1 * YEAR; // s
     static const unsigned int DUTY_CYCLE = 1000000; // ppm
 
-    static const bool reboot = true;
+    static const bool reboot = false;
 
     static const unsigned int STACK_SIZE = Traits<Machine>::STACK_SIZE;
     static const unsigned int HEAP_SIZE = (Traits<Application>::MAX_THREADS + 1) * Traits<Application>::STACK_SIZE;
